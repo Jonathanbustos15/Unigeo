9be7672b0e85a26d86c99c65f9e626ebb29a0f2f
Jeison Mauricio Zapata <jzapata@reval.local>
2019-09-04 14:43:21 -0500

    init commit

---
+++ vistas/Menup.php
@@ -0,0 +1,108 @@
+<h xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+   <head>
+      <meta http-equiv="Content-Type" content="text/h; charset=UTF-8">
+      <title>Unigeo</title>
+      <link href="componentes/css/content.css" rel="stylesheet" type="text/css"/>
+      <link href="componentes/css/basemod.css" rel="stylesheet" type="text/css"/>
+      <link href="componentes/css/Stylemenup.css" rel="stylesheet" type="text/css"/>
+      
+      			<script type="text/javascript">
+				function showTable() {
+					document.getElementById("noscript").style.display = "none";
+				}
+			</script></head>
+   <body onload="showTable();">
+      <div id="page">
+         <div id="header">
+            <div id="headline">
+               <h1>Unigeo</h1></div>
+            <div id="logo"></div>
+         </div>
+         <div id="main">
+            <div id="non-ie">
+               <table class="pages-listing full">
+                  <tr class="odd">
+                     <td>Proyecto</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>Configuración_proyecto</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>Ubicación</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>Universidad</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>Crea Universidad</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>Detalles de Universidad</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>Crear Sede</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>Detalle Sede</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>Crear Carrera</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>Noticias</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>Crea Noticia</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="even">
+                     <td>Noticia 2</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+                  <tr class="odd">
+                     <td>Ruta</td>
+                     <td><a class="btn" href="#">Opcion 1</a></td>
+                     <td><a class="btn" href="#">Opcion 2</a></td>
+                     <td><a class="btn" href="#">Opcion 3</a></td>
+                  </tr>
+               </table>
+            </div>
+            <div id="noscript">You have Javascript disabled. Please enable Javascript to see the content of this page. Thanks</div>
+         </div>
+      </div>
+   </body>
+</h>
---
+++ vistas/menu.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<title>Menu</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<script src="componentes/js/bootstrap.js" type="text/javascript"></script>
+<link href="componentes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="componentes/css/StyleMenu.css" rel="stylesheet" type="text/css"/>
+
+<body>
+    <div class="container">
+        <div class="row titulo">
+            <div class="col-sm">
+                <h1>UNIGEO</h1>
+            </div>
+        </div>
+    </div>
+    <section>
+        <div class="container">
+
+            <table class="table1 table-striped">
+                <tbody>
+                    <tr>
+                        <td>Proyecto</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Configuración_proyecto</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Ubicación</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Universidad</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Crea Universidad</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Detalles de Universidad</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Crear Sede</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Detalle Sede</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Crear Carrera</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Noticias</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Crea Noticia</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Noticia 2</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+                    <tr>
+                        <td>Ruta</td>
+                        <td>  <a class="btn" href="#">Opcion 1</a></td>
+                        <td>  <a class="btn" href="#">Opcion 2</a></td>
+                        <td>  <a class="btn" href="#">Opcion 3</a></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+
+    </section>
+
+
+</html>
+</body>
+<?php
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+

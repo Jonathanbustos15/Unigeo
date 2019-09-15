6b60bef4a719c898d05f88a47a0a6b0afc0e2338
jzapata-unigeo <jeisonmzapata@outlook.com>
2019-09-15 12:07:08 -0500

    "header1"

---
+++ vistas/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Bootstrap 4 Static Navbar</title>
+        <link href="componentes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="componentes/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="componentes/js/popper.min.js" type="text/javascript"></script>
+        <script src="componentes/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="componentes/css/Stylehead.css" rel="stylesheet" type="text/css"/>
+
+    </head>
+    <body>
+        <div class="header">
+            <nav class="navbar navbar-expand-md nav-bg">
+<!--                <div>
+                    <img src="componentes/images/Logo.png" alt=""/>
+                </div>-->
+                <button type="button" class="navbar-toggler navbar-dark" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                </button>
+                <a href="#" class="navbar-brand">
+                    <img src="componentes/images/UNIGEO1.png" alt=""/>
+                </a>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav">
+                        <a href="#" class="nav-item nav-link active">Inicio</a>
+                        <a href="#" class="nav-item nav-link">Ruta</a>
+                        <a href="#" class="nav-item nav-link">Universidad</a>
+                        <a href="#" class="nav-item nav-link">Noticias</a>
+                        <a href="#" class="nav-item nav-link">Usuarios</a>
+
+                    </div>
+                    <div class="navbar-nav ml-auto">
+                        <a href="#" class="nav-item nav-link">Nombre del usuario</a>
+                                           </div>
+                </div>
+                <img width="200" src="componentes/images/SanjoseLog2_1.png" alt=""/>
+
+            </nav>
+        </div>
+    </body>
+</html>                              

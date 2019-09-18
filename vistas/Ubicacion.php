e9186259f49356de1980d16899bffb7212211c5d
jzapata-unigeo <jeisonmzapata@outlook.com>
2019-09-17 23:37:59 -0500

    "ubicacion 1"

--- vistas/Ubicacion.php
+++ vistas/Ubicacion.php
@@ -4,30 +4,50 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <script src="componentes/js/bootstrap.js" type="text/javascript"></script>
 <link href="componentes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="componentes/css/StyleMenu.css" rel="stylesheet" type="text/css"/>
+<link href="componentes/css/Styleubicacion.css" rel="stylesheet" type="text/css"/>
 
+<?php include("header.php"); ?>
 <body>
-
-
-    <div class="container">
-        <div class="row">
-
-            <div class="col-sm">              
-            </div>
-        </div>
-    </div>
     <section class="mg">
-
-
         <div class="container">
             <div class="row">
-                <div class="col-10 bg-info a1">
+                <div class="col-4 bg-info a1">
                     <div>
-                        elemento1 
+                        <form action="action">
+                            <div class="form-group row">
+                                <label for="example-text-input" class="col-2 col-form-label">Valor</label>
+                                <div class="col-10">
+                                    <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="example-search-input" class="col-2 col-form-label">Universidad </label>
+                                <div class="col-10">
+                                    <input class="form-control" type="search" value="How do I shoot web" id="example-search-input">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="example-email-input" class="col-2 col-form-label">Sede</label>
+                                <div class="col-10">
+                                    <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="example-url-input" class="col-2 col-form-label">URL</label>
+                                <div class="col-10">
+                                    <input class="form-control" type="url" value="https://getbootstrap.com" id="example-url-input">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="example-tel-input" class="col-2 col-form-label">Telephone</label>
+                                <div class="col-10">
+                                    <input class="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input">
+                                </div>
+                            </div>
+                        </form>
                     </div>
-
                 </div>
-                <div class="col-2 bg-dark a1">
+                <div class="col-8 bg-dark a1">
                     <div>
                         elemento2 
                     </div>
@@ -37,13 +57,5 @@
     </section>
 </html>
 </body>
-<?php
 
-
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 

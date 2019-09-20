d97ca1d51641b8f4cdae681a814b5975a79730be
jzapata-Unigeo <jeisonmzapata@outlook.com>
2019-09-19 23:38:36 -0500

    ubicacion 1

--- vistas/Ubicacion.php
+++ vistas/Ubicacion.php
@@ -14,42 +14,63 @@
                 <div class="col-4 bg-info a1">
                     <div>
                         <form action="action">
-                            <div class="form-group row">
-                                <label for="example-text-input" class="col-2 col-form-label">Valor</label>
-                                <div class="col-10">
-                                    <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
                                 </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
                             </div>
-                            <div class="form-group row">
-                                <label for="example-search-input" class="col-2 col-form-label">Universidad </label>
-                                <div class="col-10">
-                                    <input class="form-control" type="search" value="How do I shoot web" id="example-search-input">
+
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
                                 </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
                             </div>
-                            <div class="form-group row">
-                                <label for="example-email-input" class="col-2 col-form-label">Sede</label>
-                                <div class="col-10">
-                                    <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
+
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
                                 </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
                             </div>
-                            <div class="form-group row">
-                                <label for="example-url-input" class="col-2 col-form-label">URL</label>
-                                <div class="col-10">
-                                    <input class="form-control" type="url" value="https://getbootstrap.com" id="example-url-input">
+
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
                                 </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
                             </div>
-                            <div class="form-group row">
-                                <label for="example-tel-input" class="col-2 col-form-label">Telephone</label>
-                                <div class="col-10">
-                                    <input class="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input">
+
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
                                 </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </div>
+
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+                            </div>
+
+                            <div class="input-group input-group-sm mb-3">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
+                                </div>
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
                             </div>
                         </form>
+
                     </div>
                 </div>
-                <div class="col-8 bg-dark a1">
+
+
+                <div class="col-6 a2">
                     <div>
-                        elemento2 
+                        <img src="componentes/images/mapa.jpg" alt=""/>
                     </div>
                 </div>
             </div>

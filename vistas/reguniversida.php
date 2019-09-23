09566bcb435cb62435e854eba407218c32622215
jzapata-unigeo <jeisonmzapata@outlook.com>
2019-09-22 23:37:51 -0500

    universidad1

---
+++ vistas/reguniversida.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<title>Menu</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<script src="componentes/js/bootstrap.js" type="text/javascript"></script>
+<link href="componentes/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="componentes/css/Styleubicacion.css" rel="stylesheet" type="text/css"/>
+
+<?php include("header.php"); ?>
+
+<body>
+ 
+   <section class="tab">
+       
+        <table class="table-hover table-responsive-sm">
+            <tbody>
+                <tr>
+                    <th colspan="3">Detalles de la carrera</th>
+                </tr>
+                <tr>
+                    <td>NOMBRE</td>
+                    <td>SNIES</td>
+                    <td>VALOR</td>
+                   
+                </tr>
+                <tr>
+                    <td>NOMBRE1</td>
+                    <td>17892</td>
+                    <td>$1564812</td>
+                   
+                </tr>
+            </tbody>
+        </table>
+</html>
+</body>
+
+

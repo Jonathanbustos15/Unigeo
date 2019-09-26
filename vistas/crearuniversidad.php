8b8501c24e51de98d5a7a0cbd6c96c5bcabb9bb5
jzapata-Unigeo <jeisonmzapata@outlook.com>
2019-09-26 07:10:45 -0500

    crear universidad

--- vistas/crearuniversidad.php
+++ vistas/crearuniversidad.php
@@ -29,9 +29,7 @@
                                 <label for="enombre">Nombre</label>
                                 <input type="email" class="form-control" id="Nombre" placeholder="Nombre">
                             </div>
-
                         </form>  
-
                         <div>
                             <select class="custom-select p-3 form" id="inputGroupSelect01">
                                 <option selected>Choose...</option>
@@ -40,7 +38,6 @@
                                 <option value="3">Three</option>
                             </select>
                         </div>
-
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
@@ -60,9 +57,7 @@
                             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                     </div>
-
                     <button type="submit" class="btn btn-primary">Login</button>
-
                 </div>
             </div>
         </div>

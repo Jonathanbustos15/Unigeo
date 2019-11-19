9577fcb2f08596a836258199e0ee7c7d37358ee0
ChristianMoreno <36908952+ChristianMoreno@users.noreply.github.com>
2019-11-18 20:07:07 -0500

    borrar

--- vistas/newEmptyPHP.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    return $_SERVER['HTTP_CLIENT_IP'];
-if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-    return $_SERVER['HTTP_X_FORWARDED_FOR'];
-echo $_SERVER['REMOTE_ADDR'];
-
-
